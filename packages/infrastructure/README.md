c1d272a7fd45474ebfa0613e8e4f0cba5261da49
BogdanDraghia <bgd.draghia@gmail.com>
2023-01-15 18:55:31 +0000

    update cdk with appsync, dynamoDB, added basic schema

--- packages/infrastructure/README.md
+++ packages/infrastructure/README.md
@@ -1,14 +1,21 @@
-# Welcome to your CDK TypeScript project
+Welcome to the infrastructure of my Project 
 
-This is a blank project for CDK development with TypeScript.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+
+
+* `npm run cdk-build`   compile typescript to js
+
+
+
+GraphQL API 
+- [ ] AWS CDK
+- [ ] Amazon DyanamoDB 
+- [ ] AWS Lambda
+- [ ] AWS AppSync
+
+
+
--- packages/infrastructure/lib/infrastructure-stack.ts
+++ packages/infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
+import * as appsync from 'aws-cdk-lib/aws-appsync';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import { Construct } from 'constructs';
+import path = require('path')
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+// Dyanmo Db instance
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, 'Api', {
+      name: 'WaterSystemApi',
+      schema: appsync.SchemaFile.fromAsset(path.join(__dirname+"/../graphql/", 'schema.graphql')),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.IAM,
+        },
+      },
+      xrayEnabled: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const demoTable = new dynamodb.Table(this, 'DemoTable', {
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+    const demoDS = api.addDynamoDbDataSource('demoDataSource', demoTable);
+
+    demoDS.createResolver('QueryGetDemosResolver', {
+      typeName: 'Query',
+      fieldName: 'getDemos',
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultList(),
+    });
+    
+    // Resolver for the Mutation "addDemo" that puts the item into the DynamoDb table.
+    demoDS.createResolver('MutationAddDemoResolver', {
+      typeName: 'Mutation',
+      fieldName: 'addDemo',
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
+        appsync.PrimaryKey.partition('id').auto(),
+        appsync.Values.projecting('input'),
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
+    });
+    
+
   }
 }
