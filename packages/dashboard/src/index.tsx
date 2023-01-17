a8eaac99a8389a9ca78788093972c9382613b0e2
BogdanDraghia <bgd.draghia@gmail.com>
2023-01-17 00:39:06 +0000

    add react router, react chartjs

--- packages/dashboard/src/index.tsx
+++ packages/dashboard/src/index.tsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
 root.render(
   <React.StrictMode>
+          <BrowserRouter>
+          <Routes>
+          <Route path="/" element={<App />}>
+            {/* 3d Structure */}
+            </Route>
+            </Routes>
+          </BrowserRouter>
     <App />
   </React.StrictMode>
 )
